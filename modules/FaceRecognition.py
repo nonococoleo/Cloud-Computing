21dce55bac85d2c44dc1b10cd241b64631ec313b
changzhi-liu <stevenlcz98@gmail.com>
2021-05-06 21:21:43 -0400

    reform face recoginition return value

--- modules/FaceRecognition.py
+++ modules/FaceRecognition.py
@@ -3,29 +3,50 @@ import boto3
 import json
 
 
-def detect_faces(photo):
+def detect_faces(bucket, photo):
     client = boto3.client('rekognition',
                           region_name='us-east-2',
                           aws_access_key_id='AKIARAQKMHEBYCZBBY5R',
                           aws_secret_access_key='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'
                           )
-    response = client.detect_faces(Image={'Bytes': photo}, Attributes=['ALL'])
+    response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':photo}},Attributes=['ALL'])
     # print(response)
-    return json.dumps(response)
+    ar = [];
+    res = dict();
+    for faceDetail in response['FaceDetails']:
+        tempDict = dict();
+        ## getting the age
+        tempDict['AgeRange'] = (faceDetail['AgeRange']['Low'] + faceDetail['AgeRange']['High']) /2
+        ## getting the gender
+        tempDict['Gender'] = faceDetail['Gender']['Value']
+        ## getting the emotion and classify as three types
+        emo = faceDetail['Emotions'][0]['Type']
+        if (emo == 'SAD' or emo == 'ANGRY' or emo == 'FEAR' or emo == 'CONFUSED' or emo == 'DISGUSTED'):
+            tempDict['Emotions'] = 'Negtive'
+        elif (emo == 'HAPPY'):
+            tempDict['Emotions'] = 'Positive'
+        else:
+            tempDict['Emotions'] = 'mild'
+        ar.append(tempDict);
+        # a valid tempDict looks like {'AgeRange': 20.0, 'Gender': 'Female', 'Emotions': 'Negtive'}
+
+    res['faceDetail'] = ar
+    return json.dumps(res)
 
 
 def main(request):
-    image_data = base64.b64decode(request["image_data"])
-    return detect_faces(image_data)
+    # image_data = base64.b64decode(request["image_data"])
+    # 记得调换对应的参数
+    return detect_faces('facebuckt','final.JPG')
 
 
 if __name__ == "__main__":
-    file = "ident.JPG"
-    with open(file, 'rb') as image:
-        x1 = image.read()
+    # file = "ident.JPG"
+    # with open(file, 'rb') as image:
+    #     x1 = image.read()
 
-    base64_bytes = base64.b64encode(x1)
-    base64_string = base64_bytes.decode('utf-8')
+    # base64_bytes = base64.b64encode(x1)
+    # base64_string = base64_bytes.decode('utf-8')
 
     res = main({"image_data": base64_string})
     print(res)
